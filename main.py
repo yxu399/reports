df5781cf2909696b2055ce0cbdbcb26001dd4a09
Emily Ramage <ramageem@oregonstate.edu>
2026-02-18 08:37:35 -0800

    basic flask functionality in main.py with placeholders

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 # Reports Generator Microservice
 # main
 
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+
+# arbitrary route for demonstration
+@app.route('/')
+def index():
+    return 'Reports Index Page'
+
+
+# arbitrary call to index()
+index()
+
+if __name__ == '__main__':
+    app.run(debug=True)
